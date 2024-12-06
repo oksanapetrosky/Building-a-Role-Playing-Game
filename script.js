47f6af5ec31921df8e89bb2e9844b9d43e9a8d07
Douglas Petrosky <douglaspetrosky@Douglass-MacBook-Pro.local>
2024-12-06 17:43:31 -0500

    Add new function sellWeaponand functions for different monsters

--- script.js
+++ script.js
@@ -123,5 +123,40 @@ function buyWeapon() {
       }
     } else {
         text.innerText = "You already have the most powerful weapon!";
+        button2.innerText = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
   }
 }
+function sellWeapon() {
+    if (inventory.length > 1) {
+      gold += 15;
+      goldText.innerText = gold;
+      let currentWeapon = inventory.shift();
+      text.innerText = "You sold a " + currentWeapon + ".";
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "Don't sell your only weapon!";
+    }
+  }
+  function fightSlime() {
+    fighting = 0;
+    goFight();
+  }
+  
+  function fightBeast() {
+    fighting = 1;
+    goFight();
+  }
+  
+  function fightDragon() {
+    fighting = 2;
+    goFight();
+  }
+  
+  function goFight() {
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
+  }
